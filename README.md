081a141afce18de42307ce0b2253d4c0e0a3f9bb
Zeeker <ezekiel.ramirez@calbaptist.edu>
2022-04-11 14:07:26 -0700

    Got the file reader to work

--- README.md
+++ README.md
@@ -12,3 +12,5 @@ https://www.youtube.com/watch?v=ih20l3pJoeU
 https://github.com/OneLoneCoder/videos/blob/master/OneLoneCoder_olcEngine3D_Part1.cpp
 
 https://www.w3schools.com/java/java_files_read.asp
+
+https://www.geeksforgeeks.org/different-ways-reading-text-file-java/#:~:text=Here%20are%20some%20of%20the%20many%20ways%20of,strings%20using%20regular%20expressions.%20...%20More%20items...%20
--- src/loadOBJ.java
+++ src/loadOBJ.java
@@ -1,5 +1,7 @@
+import java.io.*;
 import java.io.File;
 import java.io.FileNotFoundException;
+
 import java.util.Scanner;
 
 public class loadOBJ {
@@ -10,20 +12,29 @@ public class loadOBJ {
      */
     public static void main(String[] args) throws FileNotFoundException {
 //        File myObj = new File("teapot.obj");
+//        File text = new File("teapot-Copy.txt");
 //        Scanner readMe = new Scanner(myObj);
-        try {
-            File myObj = new File("teapot.obj");
-            Scanner readMe = new Scanner(myObj);
-            while (readMe.hasNext()) {
-                String data = readMe.nextLine();
-                System.out.println(data);
-            }
-            readMe.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("Error");
-            e.printStackTrace();
-        }
+
+//        try {
+//            File myObj = new File("teapot-Copy.txt");
+//            Scanner readMe = new Scanner(myObj);
+//            while (readMe.hasNext()) {
+//                String data = readMe.nextLine();
+//                System.out.println(data);
+//            }
+//            readMe.close();
+//        } catch (FileNotFoundException e) {
+//            System.out.println("Error");
+//            e.printStackTrace();
+//        }
 
 //        System.out.println("File name: " + myObj.getName());
+//        System.out.println(text.getName());
+        File file = new File("C:\\Users\\Zeeker\\IdeaProjects\\Raytrace\\src\\teapot-Copy.txt");
+        Scanner sc = new Scanner(file);
+
+        while (sc.hasNext()) {
+            System.out.println(sc.nextLine());
+        }
     }
 }
