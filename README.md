abb9ae884dd80ddd9497826fa13fa66eae0ac267
Zeeker <ezekiel.ramirez@calbaptist.edu>
2022-04-11 13:39:37 -0700

    Implemented file reading

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ https://github.com/seanrowens/oObjLoader/blob/master/com/owens/oobjloader/builde
 https://github.com/idris/raytracer
 
 https://sebastiandang.github.io/docs/cse168/RayTracing.pdf
+
+# one lone Coder
+* this resource has a 3D system, but it is implemented in C++\
+https://www.youtube.com/watch?v=ih20l3pJoeU
+https://github.com/OneLoneCoder/videos/blob/master/OneLoneCoder_olcEngine3D_Part1.cpp
+
+https://www.w3schools.com/java/java_files_read.asp
--- src/loadOBJ.java
+++ src/loadOBJ.java
@@ -1,2 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class loadOBJ {
+
+    /**
+     * this is my attempt to read the vertex and faces of the teapot.obj
+     * file
+     */
+    public static void main(String[] args) throws FileNotFoundException {
+//        File myObj = new File("teapot.obj");
+//        Scanner readMe = new Scanner(myObj);
+        try {
+            File myObj = new File("teapot.obj");
+            Scanner readMe = new Scanner(myObj);
+            while (readMe.hasNext()) {
+                String data = readMe.nextLine();
+                System.out.println(data);
+            }
+            readMe.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error");
+            e.printStackTrace();
+        }
+
+//        System.out.println("File name: " + myObj.getName());
+    }
 }
